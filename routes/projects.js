7e89254be3798e163b77f2778aac42d7a1bc9d0a
Thomas MENAGE <thomas.menage@hei.fr>
2026-04-20 17:29:14 +0200

    patch project route added

--- routes/projects.js
+++ routes/projects.js
@@ -124,4 +124,94 @@ router.delete("/:projectId", authJWT, async (req, res) => {
   }
 });
 
+// Patch  project
+router.patch("/:projectId", authJWT, async (req, res) => {
+  // req.body check
+  if (!checkBody(req.body, ["title", "sportTeam"])) {
+    return res
+      .status(400)
+      .json({ result: false, error: "Missing or empty fields" });
+  }
+
+  const {
+    title,
+    description,
+    sportTeam,
+    productEngineer,
+    kickOff,
+    feasiOk,
+    creaOk,
+    selectionOk,
+    shipmentOk,
+    industrialisation,
+    kickOffIndus,
+    goIndus,
+    trialRun,
+    pilotRun,
+    goProd,
+    status,
+  } = req.body;
+
+  // Check project in BDD
+  try {
+    const projectExisting = await Project.findOne({
+      _id: req.params.projectId,
+      owner: req.userId,
+    });
+    if (!projectExisting) {
+      return res
+        .status(404)
+        .json({ result: false, error: "Project not found" });
+    }
+  } catch (error) {
+    console.error("Error", error);
+    return res.status(502).json({
+      result: false,
+      error: "Server error when checking project, try later",
+    });
+  }
+
+  // Project modification
+  const regex = /\s+/g;
+  const slug = title.trim().toLowerCase().replace(regex, "_");
+  try {
+    await Project.updateOne(
+      { _id: req.params.projectId },
+      {
+        title,
+        slug,
+        description,
+        sportTeam,
+        productEngineer,
+        kickOff,
+        feasiOk,
+        creaOk,
+        selectionOk,
+        shipmentOk,
+        industrialisation,
+        kickOffIndus,
+        goIndus,
+        trialRun,
+        pilotRun,
+        goProd,
+        status,
+      },
+    );
+
+    // Fetch the updated project
+    const modifiedProject = await Project.findById(req.params.projectId);
+
+    res.json({
+      result: true,
+      project: modifiedProject,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(502).json({
+      result: false,
+      error: "Server error when updating project, try later",
+    });
+  }
+});
+
 module.exports = router;
