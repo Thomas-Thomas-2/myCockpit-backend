e9398dc2663364d39e1a8f7d1dc22aaae3a34c0d
Thomas MENAGE <thomas.menage@hei.fr>
2026-04-27 14:28:14 +0200

    connection postgres set

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,34 @@
-const mongoose = require("mongoose");
+// Mongodb connection
+//  const mongoose = require("mongoose");
 
-const connectionString = process.env.CONNECTION_STRING;
+// const connectionString = process.env.CONNECTION_STRING;
 
-mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log("Database connected"))
-  .catch((error) => console.error(error));
+// mongoose
+//   .connect(connectionString, { connectTimeoutMS: 2000 })
+//   .then(() => console.log("Database connected"))
+//   .catch((error) => console.error(error));
+
+// PostgreSQL / Supabase connection
+const { Pool } = require("pg");
+
+const connectionString = process.env.POSTGRES_URL;
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const testConnection = async () => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("Connection DB OK", result);
+  } catch (error) {
+    console.error("Connection error:", error);
+  }
+};
+
+testConnection();
+
+module.exports = pool;
